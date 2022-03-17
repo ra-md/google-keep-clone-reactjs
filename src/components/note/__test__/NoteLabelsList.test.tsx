6d3c9d259fccc589781284b77bce98be894c9c54
ra-md <ra799m@gmail.com>
2022-03-17 13:17:04 +0700

    d

--- src/components/note/__test__/NoteLabelsList.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen, userEvent } from "~/utils/test-utils";
-import NoteLabelsList from "../NoteLabelsList";
-
-describe('NoteLabelList', () => {
-	it('should render label list correctly', () => {
-		render(<NoteLabelsList noteId="1" labelId={["1"]}/>)
-	})
-})
