e6d0ca17abe7948d41e4a7bc7c099bea539f21d0
ra-md <ra799m@gmail.com>
2022-04-09 14:24:55 +0700

    rename

--- src/utils/find-note-by-label-id.ts
+++ src/utils/find-note-by-label-id.ts
@@ -1,7 +1,7 @@
 import { Note } from "~/types";
 
-export function findNoteByLabelId(notes: Note[], id: string) {
+export function findNoteByLabelId(notes: Note[], labelId: string) {
   return notes.filter((note) => {
-    return note.labelIds.find((id) => id === id);
+    return note.labelIds.find((id) => id === labelId);
   });
 }
